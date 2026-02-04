f10c3b7905ff4da283c324f0b9da8f3a2a520600
raph <raph@Mac-mini.local>
2026-02-04 16:18:01 +0100

    chore: typsecript config
    - creation du fichier tsconfig
    - npm build, blocage en cas de warnings
    - exclure maplibre du bundle

--- js/vite.config.js
+++ js/vite.config.js
@@ -3,8 +3,16 @@ import { defineConfig } from "vite";
 export default defineConfig({
   build: {
     lib: {
-      entry: "index.js",
+      entry: "index.ts",
       name: "coordo",
+    },
+    rollupOptions: {
+      external: ["maplibre-gl"],
+      output: {
+        globals: {
+          "maplibre-gl": "maplibregl",
+        },
+      },
     },
   },
 });
