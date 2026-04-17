99355f721b6042a35f82de45181b35624e75501e
David Bretaud <david.bretaud.dev@gmail.com>
2026-04-17 09:48:20 +0200

    feat(symbol): Allow to provide svg

--- coordo-ts/src/layers/symbol.ts
+++ coordo-ts/src/layers/symbol.ts
@@ -22,6 +22,7 @@ export function makeSetLayerSymbol({ map }: { map: MapLibreMap }) {
    * @param params.imageUrl — The URL of the image file. Image file must be in png, webp, or jpg format.
    * @param params.spriteId — The ID of the image to load from the sprite attached to the map instance.
    * @param params.iconSize — The units in factor of the original icon size
+   * @param params.svg — The SVG to use as picture.
    * @param params.fallbackId — The ID of a picture (from a sprite or from addImage)
    * to use as fallback when the main image couldn't load.
    */
@@ -31,13 +32,15 @@ export function makeSetLayerSymbol({ map }: { map: MapLibreMap }) {
     fallbackId,
     imageUrl,
     spriteId,
+    svg,
   }: {
     layerId: string;
     iconSize?: number;
     fallbackId?: string;
   } & (
-    | { imageUrl: string; spriteId?: null }
-    | { spriteId: string; imageUrl?: null }
+    | { imageUrl: string; spriteId?: null; svg?: null }
+    | { spriteId: string; imageUrl?: null; svg?: null }
+    | { svg: string; imageUrl?: null; spriteId?: null }
   )) {
     let pictureId: string = "";
 
@@ -49,6 +52,19 @@ export function makeSetLayerSymbol({ map }: { map: MapLibreMap }) {
       const image = await map.loadImage(imageUrl);
       const imageId = getLayerSymbolPictureId({ layerId });
       map.addImage(imageId, image.data);
+      pictureId = imageId;
+    }
+
+    if (svg) {
+      // Ref: https://maplibre.org/maplibre-gl-js/docs/examples/display-a-remote-svg-symbol/
+      const image = new Image();
+      const promise = new Promise((resolve) => {
+        image.onload = resolve;
+      });
+      image.src = svg;
+      await promise; // Wait for the image to load
+      const imageId = getLayerSymbolPictureId({ layerId });
+      map.addImage(imageId, image);
       pictureId = imageId;
     }
 
