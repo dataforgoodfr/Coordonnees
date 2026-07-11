fb632bcc2f5ab24ab97b86e0e5acbeddbd576c59
Arnaud Fournier <arnaudfournier921@gmail.com>
2026-07-11 11:41:21 +0200

    feat(ts-map): Add option for using headers in styleUrl requests (#97)
    * Add headers to createMapOptions to handle authorization
    
    * Send custom headers only on styleUrl
    
    * Fix lint

--- coordo-ts/src/map/map.ts
+++ coordo-ts/src/map/map.ts
@@ -25,6 +25,7 @@ export type CreateMapOptions = Partial<maplibregl.MapOptions> & {
     LayerControlConstructorProps,
     "renderAnchor" | "renderLayerRow"
   >;
+  headers?: Record<string, string>;
 };
 
 export function createMap(
@@ -49,10 +50,23 @@ export function createMap(
     ...providedMapLibreOptions,
   };
 
+  // Extract headers from CreateMapOptions
+  const customHeaders = options?.headers || {};
+
   const map = new maplibregl.Map({
     center: mergedMapLibreOptions.center,
     container: el,
     style: styleUrl,
+    // Inject transformRequest to handle headers
+    transformRequest: (url, _) => {
+      if (Object.keys(customHeaders).length > 0 && url === styleUrl) {
+        return {
+          headers: customHeaders,
+          url,
+        };
+      }
+      return { url };
+    },
     zoom: mergedMapLibreOptions.zoom,
   });
 
@@ -70,7 +84,7 @@ export function createMap(
 
   const addSprite = map.addSprite;
 
-  const setLayerFilters = makeSetLayerFilters({ baseUrl, map });
+  const setLayerFilters = makeSetLayerFilters({ baseUrl, map, options });
 
   const setLayerPopup = makeSetLayerPopup({ map });
 
